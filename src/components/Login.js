e74662cf86867eaa05cbd14023436ef51a8072b1
N-and-U <nandakumar.k05@gmail.com>
2022-08-16 15:50:17 +0530

    login test

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,69 +1,153 @@
 import React from 'react'
 import styled from 'styled-components'
 import klubg from '../static/images/klu.gif'
-import '../static/Login.css'
+import klulogo from '../static/images/klulogo.png'
+
+const Bodyconatiner = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+`;
+
+const Navbar = styled.div`
+    background-color: #A51C24;
+    width: 100%;
+    height: 5vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    cursor: pointer;
+`;
+
+const LoginA = styled.a`
+    text-decoration: none;
+    color: white;
+    display: flex;
+`;
+
+const ImgContainer = styled.div`
+    width: 100%;
+    height: 92vh;
+`;
+
+const BgImage = styled.img`
+    width: 100%;
+    height: 100%;
+`;
+
+const Footer = styled.div`
+    width: 100%;
+    height: 3vh;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    background-color: #A51C24;
+    color: white;
+`;
+
+const LoginContainer = styled.div`
+    top: 0;
+    left: 0;
+    position: absolute;
+    display: ${(props) => props.loginChoice};
+    width: 100vw;
+    height: 100vh;
+    justify-content: center
+`;
+
+const LoginHeader = styled.div`
+    width: 25vw;
+    height: 70vh;
+`;
+
+const LogoContainer = styled.div`
+    width: 100%;
+    height: 6vh;
+    background-color: #A51C24;
+    margin-top: 5vh;
+    display: flex;
+    position: relative;
+    justify-content: center;
+`;
+
+
+const LogoImgContainer = styled.div`
+    width: 60px;
+    height: 60px;
+    background-color: #A51C24;
+    border-radius: 50%;
+    margin-top: -4vh;
+    padding: 0.5%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const LogoImg = styled.img`
+    width: 55px;
+    height: 55px;
+`;
+
+const LoginClose = styled.div`
+    position: absolute;
+    right: 5px;
+    color: aliceblue;
+    cursor: pointer;
+`;
+
+
+const LoginBody = styled.div`
+    width: 20vw;
+    height: 50vh;
+    padding: 3vh 2.5vw;
+    background-color: white;
+`;
+
+const LoginFooter = styled.div`
+    width: 25vw;
+    height: 5vh;
+    text-align: center;
+    background-color: #A51C24;
+`;
 
 function Login() {
 
-  function onClickLogin(props){
-    
-    if( props == 'open' ) {
-      document.getElementsByClassName('login-container')[0].style.display = 'flex';
-  }
-  else {
-      document.getElementsByClassName('login-container')[0].style.display = 'none';
-  }
-  }
+    const [loginChoice, setLoginChoice] = React.useState('none');
 
   return (
     <React.Fragment>
-          <div class="body-conatiner">
-              <div class="navbar">
-                  <a href="#" onclick="login('open')">
-                      <i>
-                          
-                      </i>
-                      <h3>Login</h3>
-                  </a>
-              </div>
-              <div class="img-container">
-                  <img src={klubg} alt="klu" />
-              </div>
-              <div class="footer">
-                  <p>© Copyright 2021 by KLU PP . All Rights Reserved.</p>
-              </div>
-              <div class="login-container">
-
-                <div class="login-header">
-                      <div class="logo-container">
-                          <div class="logo-img-container">
-                              <img src="{% static 'images/smalllogo.png' %}" alt="klu" />
-                          </div>
-                          <div class="login-close" onclick="login('close')">
-                              &#10060;
-                          </div>
-                      </div>
-                      <div class="login-body">
-                          <form>
-                              <p>Username</p>
-                              <input type="text" name="username" id="username"/>
-                              <br/>
-                              <p>Password</p>
-                              <input type="password" name="password" id="password"/>
-                              <br/>
-                              
-                              <p id="forgot">Forgot your password?</p>
-                              <br/>
-                              <br/>
-                              <button type="submit">Login</button>
-                          </form>
-                      </div>
-                      <div class="footer">
-                          <p>© Copyright 2019 by K L Deemed to be University.</p>
-                      </div>
-                </div>
-              </div>
-            </div>
+          <Bodyconatiner>
+                <Navbar>
+                    <LoginA href="#"  onClick={()=> {setLoginChoice('flex')}}>
+                        <h3>Login</h3>
+                    </LoginA>
+                </Navbar>
+                <ImgContainer>
+                    <BgImage src={klubg} alt="klu" />
+                </ImgContainer>
+                <Footer>
+                    <p>© Copyright 2021 by KLU PP . All Rights Reserved.</p>
+                </Footer>
+                <LoginContainer loginChoice = {loginChoice}>
+                    <LoginHeader>
+                        <LogoContainer>
+                            <LogoImgContainer>
+                                <LogoImg src={klulogo} alt="klu" />
+                            </LogoImgContainer>
+                            <LoginClose  onClick={()=> {setLoginChoice('none')}}>
+                                &#10060;
+                            </LoginClose>
+                        </LogoContainer>
+                        <LoginBody>
+                            
+                        </LoginBody>
+                        <LoginFooter>
+                            <p>© Copyright 2019 by K L Deemed to be University.</p>
+                        </LoginFooter>
+                    </LoginHeader>
+                </LoginContainer>
+            </Bodyconatiner>
     </React.Fragment>
   )
 }
---
+++ src/components/LoginTest.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from "gapi-script";
+
+const clientId = "887149817062-sd5m283jb38o0hi12ksiot85bar7qae1.apps.googleusercontent.com";
+
+function LoginTest() {
+
+    gapi.load("client:auth2", () => {
+        gapi.client.init({
+          clientId:
+            "*****.apps.googleusercontent.com",
+          plugin_name: "chat",
+        });
+      });
+
+    const [showloginButton, setShowloginButton] = useState(true);
+    const [showlogoutButton, setShowlogoutButton] = useState(false);
+    const onLoginSuccess = (res) => {
+        console.log('Login Success:', res.profileObj);
+        setShowloginButton(false);
+        setShowlogoutButton(true);
+    };
+
+    const onLoginFailure = (res) => {
+        console.log('Login Failed:', res);
+    };
+
+    const onSignoutSuccess = () => {
+        alert("You have been logged out successfully");
+        console.clear();
+        setShowloginButton(true);
+        setShowlogoutButton(false);
+    };
+
+    return (
+        <div>
+            { showloginButton ?
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign In"
+                    onSuccess={onLoginSuccess}
+                    onFailure={onLoginFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                /> : null}
+
+            { showlogoutButton ?
+                <GoogleLogout
+                    clientId={clientId}
+                    buttonText="Sign Out"
+                    onLogoutSuccess={onSignoutSuccess}
+                >
+                </GoogleLogout> : null
+            }
+        </div>
+    );
+}
+export default LoginTest;
